4033df87e15e1483168a401339642475cd296787
Hiroki Takahashi <9676341+Hiroki111@users.noreply.github.com>
2022-08-17 07:20:49 +0200

    Removed old backoffice page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 
 import { Home } from './Home';
-import { BackOffice } from './BackOffice';
-import { BackOfficeLoginForm } from './BackOfficeLoginForm';
-import { withAuth } from '../hoc/withAuth';
 import { ROUTES } from '../routes';
 
 export function RootRouter() {
@@ -21,8 +18,6 @@ export function RootRouter() {
           ]}
           component={Home}
         />
-        <Route path={ROUTES.login} component={BackOfficeLoginForm} />
-        <Route path={ROUTES.backoffice} component={withAuth(BackOffice)} />
         <Redirect to={ROUTES.service} />
       </Switch>
     </Router>
